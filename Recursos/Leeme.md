4c7fb2eeda6af710c37f868f81f079933f7a9985
a.22308051281096 <a.22308051281096@cbtis128.edu.mx>
2025-05-29 15:53:01 +0000

    first commit

---
+++ Recursos/Leeme.md
@@ -0,0 +1,7 @@
+## AppBar
+
+![alt text](image-2.png)
+
+## Encabezado 
+
+![alt text](image-3.png)
---
+++ lib/homepage.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: AppBar(
+        elevation: 0.2,
+        title: Text("Formularios Tablas"),
+        centerTitle: true,
+        backgroundColor: Colors.red,
+        actions: <Widget>[
+          InkWell(
+            onTap: null,
+            child: IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.white,
+              ), onPressed: () {  },
+            ),
+          ),
+          InkWell(
+            onTap: null,
+            child: IconButton(
+              icon: Icon(
+                Icons.settings,
+                color: Colors.white,
+              ), onPressed: () {  },
+            ),
+          ),
+        ],
+      ),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              decoration: BoxDecoration(color: const Color.fromARGB(255, 233, 94, 30)),
+              accountName: Text("Cristian Salazar 1096"),
+              accountEmail: Text("a.22308051281096@cbtis128.edu.mx"),
+              currentAccountPicture: GestureDetector(
+                child: CircleAvatar(
+                  backgroundColor: Colors.white,
+                  child: Text(
+                    "AM",
+                    style: TextStyle(
+                        color: Colors.pink,
+                        fontSize: 20.0,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
